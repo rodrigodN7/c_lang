254df477410969212e4ffe5a39392fb6df4cd151
rodrigodN7 <102780110+rodrigodN7@users.noreply.github.com>
2022-04-21 22:31:35 -0500

    addition_program

---
+++ addition_program.c
@@ -0,0 +1,26 @@
+/*
+ * addition_program.c
+ *
+ * Created:
+ * Author : Rodrigo Noriega
+ * 
+ */ 
+
+#include <stdio.h>
+
+int main(void){
+
+        int integer1, integer2, sum;
+
+        printf("Enter first integer\n");
+        scanf("%d", &integer1);
+
+        printf("Enter second integer\n");
+        scanf("%d", &integer2);
+
+        sum = integer1 + integer2;
+
+        printf("Sum is %d\n", sum);
+
+    return 0;
+}
